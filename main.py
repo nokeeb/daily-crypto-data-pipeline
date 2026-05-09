24d8e2be76c71142acbdbb82509a29a6f3dc5829
Kenan Baralic <kenann.bara@gmail.com>
2026-05-09 15:07:17 +0200

    Added error handling

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from pipeline import extract,transform,load,analyze,report
 
 def run_pipeline():
     apiData=extract.fetch_crypto_data()
-    cleanedData=transform.transform_data(apiData)
-    load.save_to_csv(cleanedData,'crypto_data.csv')
-    reportData=analyze.analyze_data('crypto_data.csv')
-    report.create_report(reportData)
+    if apiData != []:
+        cleanedData=transform.transform_data(apiData)
+        load.save_to_csv(cleanedData,'crypto_data.csv')
+        reportData=analyze.analyze_data('crypto_data.csv')
+        report.create_report(reportData)
 
-run_pipeline()
+ 
+if __name__=='__main__':
+    run_pipeline()
